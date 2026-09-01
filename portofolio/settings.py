806464ec8af9d3d6c22e6bf732e105ed9648e44d
Rheina <rheina.ul67@gmail.com>
2026-09-01 23:19:59 +0700

    chore: setup whitenoise untuk static files

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -162,3 +163,5 @@ TEMPLATES = [
     
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+WHITENOISE_USE_FINDERS = True

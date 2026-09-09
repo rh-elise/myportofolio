ce96d83d1e875f0dad60b9daa8dbadcacac2597e
Rheina <rheina.ul67@gmail.com>
2026-09-09 16:05:35 +0700

    chore: Men-downgrade versi Django

--- main/models.py
+++ main/models.py
@@ -1,6 +1,30 @@
 import uuid
 from django.db import models
 
+class Projects(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.CharField(max_length=255)  # path file
+    link = models.CharField(max_length=255, blank=True)  # URL menuju game
+
+    def __str__(self):
+        return self.title
+
+class Artworks(models.Model):
+    CATEGORY_CHOICES = [
+        ('rendered', 'Rendered'),
+        ('monochrome', 'Monochrome'),
+        ('pixel', 'Pixel'),
+        ('animation', 'Animation'),
+    ]
+    title = models.CharField(max_length=255)
+    image = models.CharField(max_length=255, blank=True)  # kosong kalau berupa video
+    video = models.CharField(max_length=255, blank=True)  # kosong kalau berupa gambar
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='pixel')
+
+    def __str__(self):
+        return self.title
+    
 class Experience(models.Model):
     EXPERIENCE_CHOICES = [
         ('internship', 'Internship'),
@@ -18,6 +42,7 @@ class Experience(models.Model):
     thumbnail = models.URLField(blank=True, null=True)
     started_at = models.DateTimeField(auto_now_add=True)
     ended_at = models.DateTimeField(blank=True, null=True)
+    
     def __str__(self):
         return self.title
     
